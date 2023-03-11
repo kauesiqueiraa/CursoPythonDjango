0418aa597e2716e59240d0ef63b5df3397344737
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-03-11 14:40:05 -0300

    lista e detalhe de cidades

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from cadastros.models import Cidade
 
 
-# Create your views here.
-
 def lista_cidades(request):
 
-    pass
+    qs = Cidade.objects.all()
+
+    context = {
+        'cidades': qs,
+        'titulo': 'SIDIA'
+    }
+
+    return render(request, 'cadastros/lista_cidades.html', context)
+
+def detalhe_cidade(request, id):
+
+    # id_cidade = request.GET['id_cidade']
+
+    #cidade = Cidade.objects.get(pk=id_cidade)
+    cidade = get_object_or_404(Cidade, pk=id)
+
+    context = {
+        'cidade': cidade,
+    }
+
+    return render(request, 'cadastros/detalhe_cidades.html', context)
+
+
+
 
     # retornar a lista de cidades cadastradas...
     # DRM do Django
