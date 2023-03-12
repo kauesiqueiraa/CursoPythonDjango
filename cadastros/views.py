9150d0fd55f2e169d0cfb3e756030a814d109442
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-03-12 01:53:12 -0300

    adcionando o criar cidade e delete

--- cadastros/views.py
+++ cadastros/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from cadastros.forms import CidadeForm
 from cadastros.models import Cidade
 
 
 def lista_cidades(request):
 
-    qs = Cidade.objects.all()
+    qs = Cidade.objects.all().order_by('nome')
 
     context = {
         'cidades': qs,
@@ -27,7 +28,37 @@ def detalhe_cidade(request, id):
 
     return render(request, 'cadastros/detalhe_cidades.html', context)
 
+def remove_cidade(request, id):
 
+    cidade = get_object_or_404(Cidade, pk=id)
+
+    cidade.delete()
+
+    context = {
+        'cidade': cidade,
+    }
+
+    return redirect('cidades-list')
+
+def cadastra_cidade(request):
+
+    if request.method == 'POST':
+
+        form = CidadeForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('cidades-list')
+    else:
+        form = CidadeForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'cadastros/cadastra_cidades.html', context)
 
 
     # retornar a lista de cidades cadastradas...
